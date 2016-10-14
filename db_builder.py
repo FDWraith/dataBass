37de863eaea6732c9a43f389b8a051ec9a3d0dc0
Patrick Chan <patrickchan1999@gmail.com>
2016-10-14 12:55:02 -0400

    Modified names for greater clarity

--- db_builder.py
+++ db_builder.py
@@ -1,46 +1,47 @@
 import sqlite3   #enable control of an sqlite database
 import csv       #facilitates CSV I/O
-
+from os import path, remove
+from sys import argv
 
 f="discobandit.db"
 
+debug = False
+if len(argv) > 1 and argv[1] == "debugOn":
+    debug = True
+if debug and path.exists(f):
+    remove(f)
+    
 db = sqlite3.connect(f) #open if f exists, otherwise create
 c = db.cursor()    #facilitate db ops
 
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 #...perhaps by beginning with these examples...
-d1 = csv.DictReader(open('peeps.csv'))
-d2 = csv.DictReader(open('courses.csv'))
 
+try:
+    with open('peeps.csv') as csvFile:
+        peepsReader = csv.DictReader(csvFile)
+        createStudentsTableQuery = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
+        c.execute(createStudentsTableQuery)
+        for row in peepsReader:
+            insertStudentDataQuery = "INSERT INTO students VALUES ('"+ row['name'] + "'," + row['age'] + ',' + row['id'] + ')'
+            c.execute(insertStudentDataQuery)
 
-q = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
-c.execute(q)
-
-for row in d1:
-    q = "INSERT INTO students VALUES ('"+ row['name'] + "'," + row['age'] + ',' + row['id'] + ')'
-    #print q
-    c.execute(q)
-
-q = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
-c.execute(q)
-for row in d2:
-    q = "INSERT INTO courses VALUES ('"+ row['code'] + "'," + row['mark'] + ',' + row['id'] + ')'
-    c.execute(q)    
+    with open('courses.csv') as csvFile:
+        coursesReader = csv.DictReader(csvFile)
+        createCoursesTableQuery = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
+        c.execute(createCoursesTableQuery)
+        for row in coursesReader:
+            insertCourseDataQuery = "INSERT INTO courses VALUES ('"+ row['code'] + "'," + row['mark'] + ',' + row['id'] + ')'
+            c.execute(insertCourseDataQuery) 
+except (OSError, IOError) as e:
+    print "File not found..."
+except (Exception) as e:
+    print str(e)
+    print "Unknown exception found"
     
-
-'''
-q = "CREATE TABLE students (name TEXT, id INTEGER)"
-
-c.execute(q)    #run SQL query
-
-
-q = "CREATE TABLE courses (code TEXT, id INTEGER, mark INTEGER)"
-
-c.execute(q)
-'''
-
 #==========================================================
+
 db.commit() #save changes
 db.close()  #close database
 
