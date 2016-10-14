0fcd6e3bf09d128a434a2df36b26739e2031ea0c
Kevin Zhang <kevinzhang3702@gmail.com>
2016-10-14 12:28:25 -0400

    Did all the work

--- db_builder.py
+++ db_builder.py
@@ -10,6 +10,24 @@ c = db.cursor()    #facilitate db ops
 #==========================================================
 #INSERT YOUR POPULATE CODE IN THIS ZONE
 #...perhaps by beginning with these examples...
+d1 = csv.DictReader(open('peeps.csv'))
+d2 = csv.DictReader(open('courses.csv'))
+
+
+q = "CREATE TABLE students (name TEXT, age INTEGER, id INTEGER)"
+c.execute(q)
+
+for row in d1:
+    q = "INSERT INTO students VALUES ('"+ row['name'] + "'," + row['age'] + ',' + row['id'] + ')'
+    #print q
+    c.execute(q)
+
+q = "CREATE TABLE courses (code TEXT, mark INTEGER, id INTEGER)"
+c.execute(q)
+for row in d2:
+    q = "INSERT INTO courses VALUES ('"+ row['code'] + "'," + row['mark'] + ',' + row['id'] + ')'
+    c.execute(q)    
+    
 
 '''
 q = "CREATE TABLE students (name TEXT, id INTEGER)"
